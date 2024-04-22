788c29fbb691a48fdc2d16ecad39df29679bb307
Flemming Månsson Bornhak <fmh@vertica.dk>
2024-04-22 10:07:40 +0200

    fix mac db path

--- NFKApplication/Database/PathHelper.cs
+++ NFKApplication/Database/PathHelper.cs
@@ -2,8 +2,9 @@
 {
     public static class PathHelper
     {
-        public static string DatabaseConnectionString => @"Data Source=" + DatabasePath;
-        public static string DatabasePath => Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\nfk.db"));
+        public static string DatabasePath => Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "nfk.db"));
+        public static string DatabaseConnectionString => "Data Source=" + DatabasePath;
+
         public static string ImagesPath => Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\wwwroot\\images\\"));
         public static string SecretMatSku = "7881955"; // <-- This is a top secret door mat. We will be ruined if anyone manages to find the product detail page for this.
     }
